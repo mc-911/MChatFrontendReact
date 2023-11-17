a5f3ab5d18737668f6da2805ee70b6a3abc52740
mustapha <mconteh211@gmail.com>
2023-11-17 20:53:17 +1300

    Added jwt state to protectedRoute

--- src/Components/protectedRoute.tsx
+++ src/Components/protectedRoute.tsx
@@ -1,21 +1,24 @@
-import { Outlet, Navigate } from 'react-router-dom'
-import axios from 'axios'
-import useIsAuth from './useIsAuth';
-import {useEffect} from 'react'
+import { Outlet, Navigate } from "react-router-dom";
+import axios from "axios";
+import useIsAuth from "./useIsAuth";
+import { useEffect, useState } from "react";
 const PrivateRoutes = () => {
-  const { isAuth, setIsAuth}  = useIsAuth();
+  const { isAuth, setIsAuth } = useIsAuth();
+  const [jwt, setJwt] = useState("");
   useEffect(() => {
-        axios.post(`${process.env.REACT_APP_API_URL}/api/authCheck`).then((response) => {
-          setIsAuth(response.status === 200)
-          console.log("Authenticated")
-        }).catch((error) => {
-            console.log("Not Authenticated")
-          setIsAuth(false)
-        });
-  },[isAuth]);
-    return(
-        isAuth ? <Outlet/> : <Navigate to="/"/>
-    )
-}
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/api/authCheck`)
+      .then((response) => {
+        setIsAuth(response.status === 200);
+        setJwt(response.data.jwt);
+        console.log("Authenticated");
+      })
+      .catch((error) => {
+        console.log("Not Authenticated");
+        setIsAuth(false);
+      });
+  }, [isAuth]);
+  return isAuth ? <Outlet /> : <Navigate to="/" />;
+};
 
-export default PrivateRoutes
+export default PrivateRoutes;
