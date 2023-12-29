586e0fed292432ce521b7834336817e8af9c5ef3
Mustapha Conteh <mco176@uclive.ac.nz>
2023-12-30 01:09:07 +1300

    Add Chat menu now takes up entire screen, on less then medium screen sizes

--- src/Components/AddChat.tsx
+++ src/Components/AddChat.tsx
@@ -78,25 +78,35 @@ export function AddChat({ showModal, setShowModal, setChats }: {
     console.log({ showModal })
 
     const AddChatSelection = () => {
-        return <div className="gap-2 flex flex-col md:flex-row w-full h-full">
-            <div className="border-white bg-gray-800 grow relative group flex flex-col  cursor-pointer" onClick={(e) => {
-                setChatSection(AddChatSection.CreateChat)
-            }}>
-                <div className="text-center text-lg my-2 ">Create</div>
-                <div className="text-center text-sm text-gray-400">Make your own group chat</div>
-                <div className="relative grow">
-                    <img alt="wrench" src={OutlineWrench} className="h-3/5 absolute right-0 left-0 top-0 bottom-0 m-auto -rotate-45 group-hover:-rotate-[35deg]" />
-                    <img alt="hammer" src={OutlineHammer} className="h-[50%] absolute right-0 left-0 m-auto top-0 bottom-0 rotate-45 group-hover:rotate-[35deg]" />
-                </div>
+        return <div className="flex flex-col md:flex-row w-full h-full gap-8">
+            <div className="flex md:hidden flex-row w-full">
+                <FontAwesomeIcon
+                    onClick={() => setShowModal(false)}
+                    size="2xl"
+                    icon={icon({ name: "arrow-left" })}
+                    className="text-gray-400 hover:text-gray-100 md:hidden active:text-gray-50  cursor-pointer"
+                />
             </div>
-            <div className="border-white group bg-gray-800 grow relative hover:visible  flex flex-col cursor-pointer" onClick={(e) => {
-                setChatSection(AddChatSection.JoinChat)
-            }}>
-                <div className="text-center text-lg my-2">Join</div>
-                <div className="text-center text-sm text-gray-400">Join an existing group chat</div>
-                <div className="relative grow">
-                    <img alt="close door" src={DoorClosed} className="absolute h-3/6 left-0 right-0 top-0 bottom-0 m-auto group-hover:invisible" />
-                    <img alt="open door" src={DoorOpen} className="absolute h-[60%] left-0 right-0 top-1/4 mx-auto invisible group-hover:visible" />
+            <div className="gap-2 grow flex flex-col md:flex-row w-full ">
+                <div className="border-white bg-gray-800 grow relative group flex flex-col  cursor-pointer basis-1/2" onClick={(e) => {
+                    setChatSection(AddChatSection.CreateChat)
+                }}>
+                    <div className="text-center text-lg my-2 ">Create</div>
+                    <div className="text-center text-sm text-gray-400">Make your own group chat</div>
+                    <div className="relative grow">
+                        <img alt="wrench" src={OutlineWrench} className="h-3/5 absolute right-0 left-0 top-0 bottom-0 m-auto -rotate-45 group-hover:-rotate-[35deg]" />
+                        <img alt="hammer" src={OutlineHammer} className="h-[50%] absolute right-0 left-0 m-auto top-0 bottom-0 rotate-45 group-hover:rotate-[35deg]" />
+                    </div>
+                </div>
+                <div className="border-white group bg-gray-800 grow relative hover:visible  flex flex-col cursor-pointer basis-1/2" onClick={(e) => {
+                    setChatSection(AddChatSection.JoinChat)
+                }}>
+                    <div className="text-center text-lg my-2">Join</div>
+                    <div className="text-center text-sm text-gray-400">Join an existing group chat</div>
+                    <div className="relative grow">
+                        <img alt="close door" src={DoorClosed} className="absolute h-3/6 left-0 right-0 top-0 bottom-0 m-auto group-hover:invisible" />
+                        <img alt="open door" src={DoorOpen} className="absolute h-[60%] left-0 right-0 top-1/4 mx-auto invisible group-hover:visible" />
+                    </div>
                 </div>
             </div>
         </div>;
@@ -116,7 +126,7 @@ export function AddChat({ showModal, setShowModal, setChats }: {
     return (<>
         <div className={`absolute bg-black h-screen w-screen top-0 bottom-0 right-0 left-0 m-auto z-20 bg-opacity-50 ${showModal ? '' : 'hidden'} flex flex-row justify-center items-center`} onClick={() => { setShowModal(false); setChatSection(AddChatSection.OptionSelect) }}>
         </div>
-        <div className={`absolute h-5/6 w-3/4 md:h-3/4 md:w-2/4 p-8 top-0 bottom-0 right-0 left-0 m-auto z-30 dark:bg-gray-900 dark:text-gray-200 ${showModal ? '' : 'hidden'}`} >
+        <div className={`absolute h-full w-full md:h-3/4 md:w-1/2 md:max-w-[50rem] md:max-h-[50rem] md:min-w-[40rem] p-8 top-0 bottom-0 right-0 left-0 m-auto z-30 dark:bg-gray-900 dark:text-gray-200 ${showModal ? '' : 'hidden'}`} >
             <AddChatContent />
         </div>
     </>);
